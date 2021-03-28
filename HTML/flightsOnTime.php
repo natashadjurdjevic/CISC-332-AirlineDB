4caeeaa7806d7f8549ec7e1a68b3afbcf602bdc8
Natasha Djurdjevic <natasha.d@sympatico.ca>
2021-03-28 00:23:54 -0400

    fixed spacing

--- HTML/flightsOnTime.php
+++ HTML/flightsOnTime.php
@@ -5,7 +5,7 @@
 ?>
 
 <h2 class="ml-16 mt-12 text-4xl font-bold text-yellow-400 hover:text-gray-200">Flights</h2>
-<div class="bg-gray-300 m-16 mt-4 py-4 pl-20 rounded shadow-lg">
+<div class="bg-gray-300 m-16 mt-4 py-4 px-20 rounded shadow-lg">
     <p class="text-center text-xl pb-4 font-bold text-green-900" >The following flights arrived on time: </p>
 
     <table class="content-center border-separate border border-green-800 table-auto m-auto">
